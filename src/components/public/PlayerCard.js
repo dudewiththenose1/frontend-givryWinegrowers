cfbc5f4ae357e4d033b4a5fb0cdcd8174555c7c9
Leo <lsauvage71>
2024-12-28 15:17:34 +0000

    picture

--- src/components/public/PlayerCard.js
+++ src/components/public/PlayerCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { joueurService } from '@/_services';
 
-import { Image, Card } from '@nextui-org/react';
+import { Card, CardFooter, Image} from '@nextui-org/react';
 
 const PlayerCard = ({ joueur }) => {
     const [imageUrl, setImageUrl] = useState();
@@ -23,10 +23,18 @@ const PlayerCard = ({ joueur }) => {
 
     return (
         <div className="border-none" >
-                <div className="player-image">
-                <Card className="max-w-[300px]">
-                    <Image width={300} isZoomed src={`/api/images/`+imageUrl} alt={`${joueur.nom} ${joueur.prenom}`} />
-                     </Card>
+            <div className="player-image">
+                <Card isFooterBlurred className="border-none" radius="lg">
+
+                    <Image width={300} isZoomed src={`/im/${imageUrl}`} alt={`${joueur.nom} ${joueur.prenom}`} />
+                    <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                        <p className=" text-black/80">{`${joueur.nom} ${joueur.prenom}`}</p>
+                        <p
+                            className="text-tiny text-white bg-black/20"
+                        >
+                        </p>
+                    </CardFooter>
+                </Card>
                 </div>
             
         </div>
